0baa5e36f100807df2104a37a5107204b7d53db6
YuriiBloshchuk <bloschukkgtl@gmail.com>
2022-03-23 09:16:21 +0100

    Tylko nwd

---
+++ NWD i NWW/NWD i NWW/NWD i NWW.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+    while (a != b)
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    return a;
+}
+int main()
+{
+    int a, b;
+    cout << "Podaj dwie liczby:";
+    cin >> a >> b;
+    cout << NWD(a, b) << endl;
+    return 0;
+}
+
