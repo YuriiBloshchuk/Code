65ae772876a9bbe8205a01695c321a11bdeaa394
YuriiBloshchuk <urablosuk@gmail.com>
2022-04-06 08:56:44 +0200

    NWW i NWD

---
+++ NWD i NWW/NWD i NWW/NWW.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+    while (a != b)
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    return a;
+}
+
+int main()
+{
+    int liczba1, liczba2;
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> liczba1;
+    cout << "Podaj druga liczbe: ";
+    cin >> liczba2;
+    cout << "NWW(" << liczba1 << "," << liczba2 << ") = " << (liczba1 * liczba2) / NWD(liczba1, liczba2) << endl;
+
+    return 0;
+}
