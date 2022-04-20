425c3bdc7ac6443fe9441d4154b4e4c106e6aef5
YuriiBloshchuk <urablosuk@gmail.com>
2022-04-20 08:46:48 +0200

    Szyfr Cezara end

---
+++ Szyfr cezara/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿//algorytm.edu.pl
+#include<iostream>
+
+using namespace std;
+
+void szyfruj(int klucz, char tab[])
+{
+	int dl = strlen(tab); 
+	if (!(klucz >= -26 && klucz <= 26)) return;
+
+	if (klucz >= 0)
+		for (int i = 0; i < dl; i++)
+			if (tab[i] + klucz <= 'Z')
+				tab[i] += klucz;
+			else
+				tab[i] = tab[i] + klucz - 26;
+	else
+		for (int i = 0; i < dl; i++)
+			if (tab[i] + klucz >= 'A')
+				tab[i] += klucz;
+			else
+				tab[i] = tab[i] + klucz + 26;
+}
+
+int main()
+{
+	char tab[1001]; 
+
+	int klucz;
+
+	cout << "Podaj wyraz składający się z dużych liter: ";
+	cin >> tab;
+
+	cout << "Podaj klucz z przedziału [-26..26]: ";
+	cin >> klucz;
+
+	szyfruj(klucz, tab); 
+
+	cout << "Po zaszyfrowaniu: " << tab << endl;
+
+	szyfruj(-klucz, tab); 
+
+	cout << "Po rozszyfrowaniu: " << tab << endl;
+
+	return 0;
+}
