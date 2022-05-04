0a635629c9d4c933c50b40314a24adccd88453ff
YuriiBloshchuk <urablosuk@gmail.com>
2022-05-04 09:02:22 +0200

    Sprawdzanie anagramów

---
+++ Anagram/Anagram/Anagram.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+const short SIZE = 256;
+int main() {
+    char* a = new char[SIZE];
+    char* b = new char[SIZE];
+    cin.getline(a, SIZE);
+    cin.getline(b, SIZE);
+    sort(a, a + strlen(a));
+    sort(b, b + strlen(b));
+    cout << (strcmp(a, b) == 0 ? "YES" : "NO");
+}
